5d95c692c81a5013d793a13feae06f3121a620f9
dagi.stogov@gmail.com <dagi.stogov@gmail.com>
2019-12-09 23:17:07 +0300

    finish 4.1

--- src/main/java/fiters/UserServletFilter.java
+++ src/main/java/fiters/UserServletFilter.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = "/admin/*")
-public class RoleFilter implements Filter {
-
+@WebFilter(urlPatterns = "/user")
+public class UserServletFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
     }
 
     @Override
@@ -21,14 +21,12 @@ public class RoleFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("User");
-        System.out.println(request.getContextPath());
-        request.getContextPath();
-        if (!user.getRole().equals("admin")) {
+        if (user == null || !(user.getRole().equals("admin") || user.getRole().equals("user"))){
             HttpServletResponse response = (HttpServletResponse) servletResponse;
-            response.sendRedirect(request.getContextPath() + "/user");
-        } else {
-            filterChain.doFilter(servletRequest, servletResponse);
+            response.sendRedirect(request.getContextPath() + "/index");
+            return;
         }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
--- src/main/java/servlet/IndexServlet.java
+++ src/main/java/servlet/IndexServlet.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 
     @Override
